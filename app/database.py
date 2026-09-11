ca11e6dd5eb7fc84edf4c56a9cd0bb008010b184
Hasandeep Singh <hasancooksreal@gmail.com>
2026-09-11 19:31:27 +0530

    Database Setup

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+DATABASE_NAME = "company_management"
+
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
